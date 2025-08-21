c679031efcb88bff7cdc6e9f367803b1dd4be4fb
GlavTV <dteplyashin@forus.ru>
2025-08-22 02:21:18 +0800

    fix: set Vite outDir to dist in vite.config.ts for Vercel 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    target: "esnext",
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
@@ -51,10 +52,6 @@ export default defineConfig({
       "@radix-ui/react-accordion@1.2.3": "@radix-ui/react-accordion",
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    target: "esnext",
-    outDir: "build",
   },
   server: {
     port: 3000,
