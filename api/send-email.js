57e99f1eb54987c49bff4c23c81925cbd1e9b505
Dmitry Teplyashin <smerx620@gmail.com>
2025-08-25 23:47:40 +0800

    Fix nodemailer createTransport method and update Gmail SMTP configuration

--- api/send-email.js
+++ api/send-email.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 
 // Настройка Gmail транспорта
 function createGmailTransporter() {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.GMAIL_USER,
